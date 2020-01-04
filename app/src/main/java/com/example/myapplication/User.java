af77dafe77559ed10cf4ea04b78bb6b8ab186e60
ahmedkhattab <ahm56523@gmail.com>
2020-01-04 18:55:11 +0100

    second commit

--- app/src/main/java/com/example/myapplication/User.java
+++ app/src/main/java/com/example/myapplication/User.java
@@ -1,8 +1,47 @@
 package com.example.myapplication;
+import com.rabbitmq.client.*;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 public class User {
 
+    private final static String QUEUE_NAME = "messenger";
 
+    public void publish() throws java.io.IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Well, that's no ordinary rabbit.";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+        channel.close();
+        connection.close();
+
+    }
+
+
+    public void subscribe() throws java.io.IOException, java.lang.InterruptedException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+
+    }
 
 
 }
