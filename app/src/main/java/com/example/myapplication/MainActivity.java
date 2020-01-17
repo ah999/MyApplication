658cf448ff90dc1d4eeeddf8aadaa1892d9685c5
ahmedkhattab <ahm56523@gmail.com>
2020-01-17 15:38:10 +0100

    fourt c

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,40 +9,37 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.os.AsyncTask;
+
+import com.rabbitmq.client.*;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 
 
 import java.util.concurrent.TimeoutException;
 
 public class MainActivity extends AppCompatActivity {
+    ConnectionFactory factory = new ConnectionFactory();
+    private final static String QUEUE_NAME = "messenger";
     TextView txt;
     Button btn;
     EditText edttext;
-    User user = new User();
+    TextView textView;
+     String etext;
 
-    //    User subscriber = new User();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void Publish(View view) throws java.io.IOException, TimeoutException {
         edttext = (EditText) findViewById(R.id.editText3);
-        final String etext = (String) edttext.getText().toString();
-        Thread thread = new Thread(new Runnable() {
+        etext = (String) edttext.getText().toString();
 
-
-            @Override
-            public void run() {
-                try  {
-                    user.publish(etext);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        thread.start();
+        new Publisher().execute(etext);
 
 
         Context context = getApplicationContext();
@@ -54,24 +51,93 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void subscribe (View view)throws java.io.IOException, java.lang.InterruptedException, TimeoutException{
+
+    public void subscribe(View view) throws java.io.IOException, java.lang.InterruptedException, TimeoutException {
         Context context = getApplicationContext();
-        CharSequence text = "subscribed ";
-        Thread thread = new Thread(new Runnable() {
+        final CharSequence text = "subscribed ";
+        txt = (TextView) findViewById(R.id.textView);
 
-            @Override
-            public void run() {
-                try  {
-                    user.subscribe();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        new Subscriber().execute();
+        new Subscriber().onPostExecute(etext);
 
-        thread.start();
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
     }
+
+    public class Publisher extends AsyncTask<String, Void, String> {
+
+
+        private Exception exception;
+        private final static String QUEUE_NAME = "messenger";
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                factory.setHost("192.168.2.104");
+                factory.setPort(5672);
+                factory.setVirtualHost("/");
+
+                factory.setUsername("ahmed");
+                factory.setPassword("123");
+
+                Connection connection = factory.newConnection();
+                Channel channel = connection.createChannel();
+                channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+                channel.basicPublish("", QUEUE_NAME, null, strings[0].getBytes());
+                channel.close();
+                connection.close();
+            } catch (TimeoutException | java.io.IOException e) {
+                return null;
+            }
+
+            return strings[0];
+        }
+
+        // run your networking code here
+
+
+    }
+    public class Subscriber extends AsyncTask< String, Void, String> {
+        private Exception exception;
+        String message;
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try{
+                factory.setUsername("ahmed");
+                factory.setPassword("123");
+                factory.setHost("192.168.2.104");
+                factory.setPort(5672);
+                Connection connection = factory.newConnection();
+                Channel channel = connection.createChannel();
+                channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+                System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+                Consumer consumer = new DefaultConsumer(channel) {
+                    @Override
+                    public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                            throws IOException {
+                        message = new String(body, "UTF-8");
+                        System.out.println(" [x] Received '" + message + "'");
+                    }
+                };
+                channel.basicConsume(QUEUE_NAME, true, consumer);
+
+            }catch (java.io.IOException| TimeoutException e){
+                return null;
+            }
+            return message;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            txt.setText(result);
+            super.onPostExecute(result);
+        }
+
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/myapplication/Subscriber.java
@@ -0,0 +1,8 @@
+package com.example.myapplication;
+
+
+import java.io.IOException;
+import android.os.AsyncTask;
+import com.rabbitmq.client.*;
+import java.util.concurrent.TimeoutException;
+
